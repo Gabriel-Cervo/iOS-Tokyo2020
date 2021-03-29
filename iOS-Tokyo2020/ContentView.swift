7d404d88730d1123a02184f68de90b2f3477c893
Gabriel-Cervo <joaogabrieldouradocervo@gmail.com>
2021-03-29 14:44:42 -0300

    Envio inicial dos arquivos

---
+++ iOS-Tokyo2020/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOS-Tokyo2020
+//
+//  Created by Joao Gabriel Dourado Cervo on 29/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS-Tokyo2020/iOS_Tokyo2020App.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Tokyo2020App.swift
+//  iOS-Tokyo2020
+//
+//  Created by Joao Gabriel Dourado Cervo on 29/03/21.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Tokyo2020App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
