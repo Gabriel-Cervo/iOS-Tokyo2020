a82baeb40a429f560f08c885cd24a798d14e8c3f
Gabriel-Cervo <joaogabrieldouradocervo@gmail.com>
2021-03-31 14:35:33 -0300

    Refactored feedView

--- iOS-Tokyo2020/View/FeedHome/feedView.swift
+++ iOS-Tokyo2020/View/FeedHome/feedView.swift
@@ -17,43 +17,19 @@ struct feedView: View {
         VStack(alignment:.leading, spacing:8) {
             Text("Sports")
                 .font(.system(size:18))
-                .fontWeight(.semibold)//semibold ??? ou medium??
+                .fontWeight(.semibold)
                 .foregroundColor(Color(red: 32 / 255, green: 43 / 255, blue: 90 / 255))
             
-            ScrollView( .horizontal, showsIndicators:false){
+            ScrollView( .horizontal, showsIndicators:false) {
                 HStack {
-                    Image("2018-10-14-Tennis-Womens-Singles-1")
-                        .resizable()
-                        .clipShape(Circle())
-                        .overlay(Circle().stroke(Color.black, lineWidth: 1))
-                        .frame(width: 83, height: 83)
-                        .opacity(0.7)
-                        .padding(3)
+                    FeedImage(imageName: "2018-10-14-Tennis-Womens-Singles-1")
                         .foregroundColor(.blue)
                     
-                    Image("21-08-2016-Basketball-7")
-                        .resizable()
-                        .clipShape(Circle())
-                        .overlay(Circle().stroke(Color.black, lineWidth: 1))
-                        .frame(width: 83, height: 83)
-                        .opacity(0.7)
-                        .padding(3)
+                    FeedImage(imageName: "21-08-2016-Basketball-7")
                     
-                    Image("2018-10-09-beach volleyball-2")
-                        .resizable()
-                        .clipShape(Circle())
-                        .overlay(Circle().stroke(Color.black, lineWidth: 1))
-                        .frame(width: 83, height: 83)
-                        .opacity(0.7)
-                        .padding(3)
+                    FeedImage(imageName: "2018-10-09-beach volleyball-2")
                     
-                    Image("04-08-2016-Football-Men-13")
-                        .resizable()
-                        .clipShape(Circle())
-                        .overlay(Circle().stroke(Color.black, lineWidth: 1))
-                        .frame(width: 83, height: 83)
-                        .opacity(0.7)
-                        .padding(3)
+                    FeedImage(imageName: "04-08-2016-Football-Men-13")
                 
                 }
                 
