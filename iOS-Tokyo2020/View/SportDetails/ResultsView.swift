23d94b0a94d30a3a8858386b34dd4ec0cf9a2456
Gabriel-Cervo <joaogabrieldouradocervo@gmail.com>
2021-04-05 14:20:01 -0300

    ATT next games and results views

--- iOS-Tokyo2020/View/SportDetails/ResultsView.swift
+++ iOS-Tokyo2020/View/SportDetails/ResultsView.swift
@@ -8,18 +8,27 @@
 import SwiftUI
 
 struct ResultsView: View {
+    var category: String
+    var athleteName: String
+    var athleteImageName: String
+    
     var body: some View {
         ZStack {
             BackgroundRectangle(width: 180, height: 140, color: Color(.white))
             
             VStack {
-                CustomFont(text: "SINGLES WOMAN", fontSize: 14, fontWeight: .semibold)
+                CustomFont(text: "SINGLES \(category)", fontSize: 14, fontWeight: .semibold)
                     .padding(.bottom, 10)
                 
                 HStack {
-                    AthleteMiniPreview()
+                    AthleteMiniPreview(imageName: athleteImageName, athleteName: athleteName)
                     
-                    Text("Medal")
+                    Image("GoldMedal")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 25, height: 25)
+                        .padding(.top, -7)
+                        .padding(.leading, 5)
                 }
             }
         }
@@ -28,6 +37,6 @@ struct ResultsView: View {
 
 struct ResultsView_Previews: PreviewProvider {
     static var previews: some View {
-        ResultsView()
+        ResultsView(category: "WOMAN", athleteName: "Serena Williams", athleteImageName: "serena-williams-promo-e1569012559666")
     }
 }
